b2c3ef9f6996b4e27ae549b8baec90b2343af2ae
Branco Dortangs <brancodortangs@hotmail.com>
2023-12-02 13:05:26 +0100

    feat(prime ng): adds prime ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, BrowserAnimationsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
